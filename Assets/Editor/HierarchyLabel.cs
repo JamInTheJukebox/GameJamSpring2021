52d6548ceaa1356d66797871aadfab2e8644e67c
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2021-02-02 00:13:20 -0600

    BASE unity Project
    Photon Bolt
    TextmeshPro
    Universal Render Pipeline

---
+++ Assets/Editor/HierarchyLabel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+
+namespace Project2020
+{
+    using UnityEngine;
+    [InitializeOnLoad]
+    public class HierarchyLabel : MonoBehaviour
+    {
+        static HierarchyLabel()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+        }
+
+        static void HierarchyWindowItemOnGUI(int instanceID, Rect SelectionRect)
+        {
+            GameObject obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+            if(obj != null && obj.name.StartsWith("---", System.StringComparison.Ordinal))
+            {
+                EditorGUI.DrawRect(SelectionRect, Color.grey);
+                EditorGUI.DropShadowLabel(SelectionRect, obj.name.Replace("-", "").ToString());
+            }
+            else if(obj != null && obj.name.StartsWith(">>>",System.StringComparison.Ordinal))
+            {
+                EditorGUI.DrawRect(SelectionRect, Color.HSVToRGB(0,0,0));
+                EditorGUI.DropShadowLabel(SelectionRect, obj.name.Replace(">", "").ToString());
+            }
+        }
+    }
+}
+
